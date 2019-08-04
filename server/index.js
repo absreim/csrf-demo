d870afd7809a6bd1386ea14ed783a93dbae801a8
Brook Li <absreim@github.com>
2019-08-04 14:01:07 -0400

    Added port config from environment variable if it exists.

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const csurf = require('csurf')
 const router = require('./routes')
 const secured = require('./routes/secured')
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
